99635952a2aae4b4e97ab275236489f5607cae24
nitishseh <136986361+nitishseh@users.noreply.github.com>
2023-06-18 21:20:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = 26506890
+API_HASH     = "5839207dcf3b270c58d88d5de6830bac"
+BOT_TOKEN    = "6097261653:AAEhtMJ26A82bfeJwcSz33jKZ2KY9O5RXE4"
 SESSION      = ""
-DATABASE_URI = ""
+DATABASE_URI = "mongodb+srv://yahar78986:nitish@cluster0.im1knla.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001909483981
 ADMIN        = 1314385986
 CHANNEL      = "@asdfzxcqwertgv"
