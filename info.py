be6e62a48ef3fdb738a75481ccdb5f780c74d427
nitishseh <136986361+nitishseh@users.noreply.github.com>
2023-06-19 16:32:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,6 @@ API_HASH     = "6e9d9da7628cd9c0867dc5a7e8b3ba67"
 BOT_TOKEN    = "6097261653:AAEhtMJ26A82bfeJwcSz33jKZ2KY9O5RXE4"
 SESSION      = "BQAcMulgaIjNdPkhB4wQ78Q_62ziMfThq79ikx2npa6fBgZsQFd_fOek6KxbWY3b7Xom1No35F1ioz05t1URzRsJe17JAgc-G-g9r20XS5XN_6vwqn1NeyofSvfTq3v1lERLtGe6mGTfXJFa-KvyetC4jxohx_kUjOkMdQ3PwjRYH9N-05L9m0Wop90TbeK15b-FfoSrmWrEOjTuFWHAfYOR99kJlXKn2RsHS5m4YJbigGRuovsnadtEEyJjBciyjfxgd8DTBkscV5cTxnMXCS1QBlb4RG0j1NkutjQjHe6S60hCidFLWRobQQVCr5sb5NO8QKaa7x4Jxcy6Z6_u9PgTAAAAATRtdQwA"
 DATABASE_URI = "mongodb+srv://yahar78986:nitish@cluster0.im1knla.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001909483981
-ADMIN        = 1314385986
+LOG_CHANNEL  = '-1001909483981'
+ADMIN        = '1314385986'
 CHANNEL      = "@asdfzxcqwertgv"
